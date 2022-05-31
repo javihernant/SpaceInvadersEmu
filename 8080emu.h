7a4dad2a2afdb1b206f77784d807ed15aec80a21
Javi <javihernant@gmail.com>
2022-05-31 18:41:51 +0200

    Change Emulate8080Op function to return number of cycles per intruction

--- 8080emu.h
+++ 8080emu.h
@@ -9,7 +9,7 @@ ConditionCodes InitCC();
 State8080 StateCreat(uint8_t *buff);
 void printState(State8080 *st);
 void UnimplementedInstruction(State8080 *state);
-void Emulate8080Op(State8080 *state);
+int Emulate8080Op(State8080 *state);
 int Disassemble8080Op(unsigned char *codebuffer, int pc);
 
 void CALL(State8080 *st, uint8_t h, uint8_t l);
